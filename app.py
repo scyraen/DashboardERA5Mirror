265b82131c569b22c640cca3beab6dc59342e6fb
Yasemin Mutlugil <yaseminmutlugil@hotmail.com>
2026-01-28 22:03:43 +0100

    added info page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from src.modules.gee_interface import initialize_gee
-from src.modules.views import charts, map_comp, map_evo
+from src.modules.views import charts, map_comp, map_evo, info
 
 
 def main():
@@ -12,15 +12,17 @@ def main():
         st.Page(map_comp.render, icon=":material/compare:", title="Map Comparison", url_path="map-comparison"),
         st.Page(charts.render, icon=":material/bar_chart:", title="Chart Analysis", url_path="chart-analysis"),
         st.Page(map_evo.render, icon=":material/history:", title="Map Evolution", url_path="map-evolution"),
+        st.Page(info.render, icon=":material/info:", title="Info", url_path="info"),
     ]
 
     current_page = st.navigation(pages, position="hidden")
 
     with st.container():
-        cols = st.columns([1, 1, 1, 1, 1])
+        cols = st.columns([1, 1, 1, 1, 1, 1])
         cols[1].page_link(pages[0], use_container_width=True)
         cols[2].page_link(pages[1], use_container_width=True)
         cols[3].page_link(pages[2], use_container_width=True)
+        cols[4].page_link(pages[3], use_container_width=True)
         st.divider()
 
     current_page.run()
---
+++ src/modules/views/info.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+def render():
+    """Display helpful project information."""
+    st.title("Info")
+    st.markdown(
+        """
+
+        ERA5 Hourly Weather Data Dashboard is a collaborative university project to visualize global weather patterns using the ERA5 dataset via Google Earth Engine (GEE), built with Streamlit and Plotly.
+
+        ## Data Source
+
+        - Website: https://dashboard-era5.streamlit.app/
+        - Dataset: https://cds.climate.copernicus.eu/datasets/reanalysis-era5-single-levels-monthly-means?tab=overview/
+
+        ## Contact Information
+
+        Students involved:
+        - Eren Kocadag (eren.kocadag@fu-berlin.de)
+        - Constantin Tomei (constantin.tomei@fu-berlin.de)
+        - Ayse Yasemin Mutlugil (yasemin.mutlugil@fu-berlin.de)
+        """
+    )
