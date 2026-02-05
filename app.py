6dee66abcc4d55c86e629643ab28bbce1549b272
Constantin Tomei <constantin.tomei@web.de>
2026-02-05 08:40:16 +0100

    removed evo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from src.modules.gee_interface import initialize_gee
-from src.modules.views import charts, info, map_comp, map_evo
+from src.modules.views import charts, info, map_comp
 
 
 def main():
@@ -11,7 +11,6 @@ def main():
     pages = [
         st.Page(map_comp.render, icon=":material/compare:", title="Map Comparison", url_path="map-comparison"),
         st.Page(charts.render, icon=":material/bar_chart:", title="Dashboard Levels", url_path="dashboard-levels"),
-        st.Page(map_evo.render, icon=":material/history:", title="Map Evolution", url_path="map-evolution"),
         st.Page(info.render, icon=":material/info:", title="Info", url_path="info"),
     ]
 
@@ -20,9 +19,8 @@ def main():
     with st.container():
         cols = st.columns([1, 1, 1, 1, 1, 1])
         cols[1].page_link(pages[0], use_container_width=True)
-        cols[2].page_link(pages[1], use_container_width=True)
-        cols[3].page_link(pages[2], use_container_width=True)
-        cols[4].page_link(pages[3], use_container_width=True)
+        cols[3].page_link(pages[1], use_container_width=True)
+        cols[5].page_link(pages[2], use_container_width=True)
         st.divider()
 
     current_page.run()
